828ae01f0c83367dd0ebcce3d9834fd99f1b0725
Adam Palmblad <adam.palmblad@teampages.com>
2010-01-20 14:59:13 -0800

    Add page caching methods to the right class.

--- init.rb
+++ init.rb
@@ -5,6 +5,7 @@ if RAILS_ENV == "test"
 
   ActionController::Base.perform_caching = true
   ActiveSupport::TestCase.send(:include, CacheTest::PageCacheTest::IntegrationTestMethods)
+  ActionController::Base.send( :include, CacheTest::PageCacheTest::ClassCachingMethods )
   ActiveSupport::TestCase.send(:include, CacheTest::FragmentCacheTest::Assertions)
   ActiveSupport::TestCase.send( :define_method, :cache_store ) do
     ActionController::Base.cache_store
